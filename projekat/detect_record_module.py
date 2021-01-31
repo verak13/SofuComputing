e13e0faacbf4173ed5eb13b8cf7c4457ccf222f4
nikolappc <neekolapapic@gmail.com>
2021-01-31 20:03:12 +0100

    Napravljen cnn model za prepoznavanje pocetka i kraja pesme

--- projekat/detect_record_module.py
+++ projekat/detect_record_module.py
@@ -1,24 +1,30 @@
 import math
 import os
-from time import sleep
+import threading
+from time import sleep, time
 
 import cv2
+import d3dshot as d3dshot
 import matplotlib.pyplot as plt
 import numpy as np
 import pyscreenshot as ImageGrab
 from PIL import Image
-from PIL.Image import Image
+from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, LeakyReLU, Flatten, Dense, MaxPool2D
-from tensorflow.keras.losses import binary_crossentropy
-from tensorflow.keras.metrics import Accuracy, BinaryAccuracy, MeanSquaredError
+from tensorflow.keras.losses import binary_crossentropy, categorical_crossentropy, mean_absolute_error, \
+    mean_squared_error
+from tensorflow.keras.metrics import Accuracy, BinaryAccuracy, MeanSquaredError, MeanAbsoluteError
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow_core.python.keras.utils import Sequence
+from playsound import playsound
 
 BATCH = 8
 TRAIN = 86
 VALIDATE = 87
-EPOCHS = 200
+TEST = 51
+EPOCHS = 40
+LEARNING_RATE = 0.03
 
 
 def configure_cnn():
@@ -59,30 +65,31 @@ def configure_cnn():
     model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
     # 380x140
-    model.add(Conv2D(32, (3, 3), (2, 2), padding="same"))
+    model.add(Conv2D(32, (3, 3), padding="same"))
     model.add(LeakyReLU())
-    model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
-    model.add(Conv2D(32, (3, 3), (2, 2), padding="same"))
+    model.add(Conv2D(32, (3, 3), padding="same"))
     model.add(LeakyReLU())
-    model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
-    model.add(Conv2D(32, (3, 3), (2, 2)))
+    model.add(Conv2D(32, (3, 3), padding="same"))
+    model.add(LeakyReLU())
+
+    model.add(Conv2D(32, (3, 3), padding="same"))
     model.add(LeakyReLU())
     model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
     # 380x140
-    model.add(Conv2D(64, (3, 3), (2, 2), padding="same"))
+    model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(LeakyReLU())
-    model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
-    model.add(Conv2D(64, (3, 3), (2, 2), padding="same"))
+    model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(LeakyReLU())
-    model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
 
-    model.add(Conv2D(64, (3, 3), (2, 2), padding="same"))
+    model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(LeakyReLU())
-    model.add(MaxPool2D((2, 2), strides=(1, 1), padding="same"))
+
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(LeakyReLU())
 
     model.add(Flatten())
     model.add(Dense(16))
@@ -90,6 +97,9 @@ def configure_cnn():
     model.add(Dense(8))
     model.add(LeakyReLU())
     model.add(Dense(2))
+
+    model.add(LeakyReLU())
+    model.add(Dense(1))
 
     model.build()
 
@@ -118,9 +128,24 @@ class Generator(Sequence):
         np.random.shuffle(self.indices)
 
 
-def open_data(length, path="./data/"):
+def extract_yellow(img):
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    hsv_color1 = np.asarray([20, 100, 100])
+    hsv_color2 = np.asarray([30, 255, 255])
+
+    img = cv2.inRange(img_hsv, hsv_color1, hsv_color2)
+
+    img = np.expand_dims(img, axis=2)
+    img = np.float32(img)
+    return img
+
+
+def open_data(length, path="./data/", single_color=False):
     x = np.empty((length, 140, 380, 1))
-    y = np.empty((length, 2))
+    # x = np.empty((length, 140, 380, 3))
+    # y = np.empty((length, 2))
+    y = np.empty(length)
     classes = []
     with open(path + "classes.txt") as f:
         for line in f.readlines():
@@ -131,45 +156,42 @@ def open_data(length, path="./data/"):
                 pass
 
     for i in range(length):
-        img = cv2.imread(path + "{}.png".format(i + 1))
+        img = cv2.imread(path + "{}.png".format(i))
         # rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        hsv_color1 = np.asarray([20, 100, 100])  # white!
-        hsv_color2 = np.asarray([30, 255, 255])  # yellow! note the order
-
-        mask = cv2.inRange(img_hsv, hsv_color1, hsv_color2)
-
-        img = np.expand_dims(mask, axis=2)
+        if single_color:
+            img = extract_yellow(img)
 
         # cv2.imwrite("LMAO.png", mask)
         #
         # plt.imshow(mask)  # this colormap will display in black / white
         # plt.show()
         # sleep(1)
+
         # plt.imshow(rgb)
         # plt.show()
         # cv2.imshow("d", rgb)
 
         x[i] = img
-        y[i] = np.zeros(2)
-        y[i][classes[i]] = 1
+        # y[i] = np.zeros(2)
+        # y[i][classes[i]] = 1
+        y[i] = classes[i]
 
     return x, y
 
 
-def train_model():
-    x, y = open_data(TRAIN, "./data/")
-    x_val, y_val = open_data(VALIDATE, "./validate_data/")
+def train_model(single_color=False):
+    x, y = open_data(TRAIN, "./data/", single_color)
+    x_val, y_val = open_data(VALIDATE, "./validate_data/", single_color)
     generator = Generator(x, y, BATCH)
 
     model = configure_cnn()
 
     model.compile(
-        optimizer=Adam(learning_rate=0.03),
-        loss=binary_crossentropy,
-        metrics=[BinaryAccuracy(), MeanSquaredError()],
+        optimizer=Adam(
+            # learning_rate=LEARNING_RATE
+        ),
+        loss=mean_absolute_error,
+        metrics=[Accuracy(), BinaryAccuracy(), MeanSquaredError(), MeanAbsoluteError()],
     )
 
     history = model.fit_generator(generator,
@@ -178,15 +200,19 @@ def train_model():
                                   shuffle=True,
                                   epochs=EPOCHS)
 
-    model.save("MODEL")
+    model.save("MODEL.h5")
 
 
-def grab_interesting_area():
+def grab_interesting_area(d3d):
+    pil = d3d.screenshot()
+    img = np.asarray(pil)
+    # cv2.imshow("image", img)
+    # cv2.waitKey()
     # img = ImageGrab.grab(bbox=(760, 470, 1140, 610))
-    img = ImageGrab.grab(bbox=(760, 520, 1140, 660))
+    # img = ImageGrab.grab(bbox=(760, 520, 1140, 660))
     # img = ImageGrab.grab(bbox=(760, 570, 1140, 710))
     # img.show()
-    return img
+    return img[520:660, 760:1140]
 
 
 def generate_data(delay=0.3, duration=60, start_delay=3, path="./data/"):
@@ -198,11 +224,12 @@ def generate_data(delay=0.3, duration=60, start_delay=3, path="./data/"):
     sleep(1)
     index = 0
     time = 0
+    d3d = d3dshot.create()
     while time < duration:
         # img = pyautogui.screenshot()
 
         # part of the screen
-        img = grab_interesting_area()
+        img = grab_interesting_area(d3d)
         img.save(path + "{}.png".format(index))
 
         # img = np.asarray(img)
@@ -232,10 +259,125 @@ def order_files_by_number(path):
         img.save(path + str(i) + ".png")
 
 
+def load_model(path="./MODEL.h5"):
+    model = keras.models.load_model(path)
+    return model
+
+
+def test_model(path="./MODEL.h5", single_color=False):
+    model = load_model(path)
+    x_test, y_test = open_data(TEST, "./test_data/", single_color)
+
+    print("Evaluate on test data")
+    results = model.evaluate(x_test, y_test, batch_size=BATCH)
+    print("test loss, test acc:", results)
+
+
+def predict_model(path):
+    model = load_model(path)
+
+    x, y = open_data(TRAIN, "./data/")
+    x_val, y_val = open_data(VALIDATE, "./validate_data/")
+
+    res1 = model.predict_classes(x)
+    print(res1)
+    res1 = model.predict(x)
+    print(res1)
+
+    res1 = model.predict(x_val)
+    print(res1)
+    res2 = model.predict_classes(x_val)
+    print(res2)
+
+
+def hough(images):
+    length = len(images)
+    results = np.empty(length)
+    i = 0
+    for img in images:
+        # if img.empty():
+        #     print(i)
+        print(np.count_nonzero(img))
+        try:
+            circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 1)
+            if circles is not None:
+                results[i] = 1
+                circles = np.uint16(np.around(circles))
+                for i in circles[0, :]:
+                    cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            results[i] = 0
+        except Exception as e:
+            print(e)
+    return results
+
+
+def record_thread(model, single_color=False):
+    d3d = d3dshot.create()
+    recording = False
+    samples = np.empty((1, 140, 380, 1))
+    while True:
+        t1 = time()
+        arr = grab_interesting_area(d3d)
+        if single_color:
+            arr = extract_yellow(arr)
+        # cv2.imshow("dis", arr)
+        # cv2.waitKey()
+        samples[0] = arr
+        res = model.predict_classes(samples)
+        print(res[0][0])
+        if res[0] == 0 and recording:
+            recording = False
+            st = threading.Thread(target=end)
+            st.daemon = True  # Daemonize thread
+            st.start()
+            print("ENDED")
+        elif res[0] == 1 and not recording:
+            recording = True
+            st = threading.Thread(target=start)
+            st.daemon = True  # Daemonize thread
+            st.start()
+            print("STARTED")
+        # sleep(0.04)
+        print(time() - t1)
+
+
+def start():
+    playsound("./start.mp3")
+
+
+def end():
+    playsound("./end.mp3")
+
+
+def real_time_detection(model_path, start_delay=3, single_color=False):
+    print("Starting recording in:")
+    for i in range(start_delay):
+        sleep(1)
+        print(3 - i)
+
+    sleep(1)
+
+    model = load_model(model_path)
+    record_thread(model, single_color=single_color)
+
+
 if __name__ == '__main__':
     # configure_cnn()
     # generate_data()
-    # generate_data(path="./validate_data/")
+    # generate_data(path="./test_data/")
+    # order_files_by_number("./test_data/")
+    # train_model()
 
-    # dafuck("./validate_data/")
-    train_model()
+    # test_model("./MODEL.973.COLOR.h5")
+
+    # predict_model(path="./MODEL.973.COLOR.h5")
+    # x, y = open_data(15)
+    # hough(x)
+
+    # test_model("./MODEL.1.h5", single_color=True)
+
+    # real_time_detection("./MODEL.987.COLOR.h5")
+    real_time_detection("./MODEL.1.h5", single_color=True)
